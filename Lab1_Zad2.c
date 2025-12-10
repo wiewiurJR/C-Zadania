090bec40b7b46f4e79cfddab10a571f89506e13f
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2025-12-10 18:27:44 +0000

    zad 2 gotowe

--- Lab1_Zad2.c
+++ Lab1_Zad2.c
@@ -4,26 +4,21 @@
 
 #include <stdio.h>
 int main() {
-    int Number[] = {};
-    int BigNumb;
+    char max;
+    char given;
 
 
+printf("Please enter a character: ");
+    scanf(" %c", &max);
 
+    for (int i=0 ; i<=1 ; i++) {
+        printf("Please enter another character: ");
+        scanf(" %c", &given);
 
+        if (given>max) {
+            max=given;
 
-
-    scanf("%d",&Number[0]);
-    printf("First Number: %d \n",Number[0]);
-    scanf("%d",&Number[1]);
-    printf("Second Number: %d \n",Number[1]);
-    scanf("%d",&Number[2]);
-    printf("Third Number: %d \n",Number[2]);
-    if (Number[0] > Number[1] && Number[0]> Number[2]){  BigNumb = Number[0]; }
-
-    else if (Number[1] > Number[2] && Number[1]>Number[0]){ BigNumb = Number[1]; }
-
-    else{ BigNumb = Number[2]; }
-
-
-    printf("The biggest Number is: %d",BigNumb);
+        }
+    }
+    printf("The last letter in alphabet is : %c",max);
 }
