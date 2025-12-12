0ac4fd7d9512dee8ea81cbc285f62beb69a91e2f
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2025-12-12 16:06:33 +0000

    test

--- Lab1_Zad7.c
+++ Lab1_Zad7.c
@@ -1,7 +1,19 @@
+#include <stdio.h>
 //
 // Created by bartosz on 12/11/2025.
 int main() {
+float a = 0;
+printf("Enter a number:");
+    scanf( " %f",&a);
+int c = a/2;
+if ((a/c)==0) {
+    printf("The number is even");
+}else {
+    printf("The number is odd");
+}
 
 
+
+01000
     
 }
