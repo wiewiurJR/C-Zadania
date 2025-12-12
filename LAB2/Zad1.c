dd1011dd6fae538ffed97ce69f3beb5050e70a46
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2025-12-12 20:55:02 +0000

    1

--- LAB2/Zad1.c
+++ LAB2/Zad1.c
@@ -1,3 +1,23 @@
 //
 // Created by bartosz on 12/12/2025.
 //
+#include <stdio.h>
+/* program sprawdza czy podana liczba jest parzysta */
+int main()
+{
+    int x;
+    printf("Podaj liczbe calkowita: ");
+    scanf("%d", &x);
+    if( x % 2 == 0 )
+    {
+        printf("Liczba %d jest parzysta\n", x);
+    }
+    else if (x % 3 == 0)
+    {
+        printf("Liczba %d jest podzielna przez 3 \n", x);
+    }else
+    {
+        printf("Liczba %d jest niepodzielna przez 2 i 3 \n", x);
+    }
+    return 0;
+}
