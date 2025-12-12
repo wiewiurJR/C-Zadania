7d119556533b016e68d1e6a616868177b51b6677
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2025-12-12 19:49:33 +0000

    ZADANIA

--- Lab1_Zad8.c
+++ Lab1_Zad8.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
 //
 // Created by bartosz on 12/12/2025.
 //
+int main() {
+    unsigned char a;
+    a=a-1;
+    printf("%hhu",a);
+    // 255 max pojemność unsigned chara więc cofając się idziemy na samąg góre, nie ma ujemnych wartości
+}
---
+++ Lab1_Zad9.c
@@ -0,0 +1,4 @@
+//
+// Created by bartosz on 12/12/2025.
+//
+// 0_0
