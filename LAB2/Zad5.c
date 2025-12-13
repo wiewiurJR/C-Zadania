ff8b7124fad079a83243c11b2e3820abcc104039
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2025-12-13 12:19:37 +0100

    zad5

--- LAB2/Zad5.c
+++ LAB2/Zad5.c
@@ -4,24 +4,38 @@
 //
 int main() {
 double x;
-double b;
+int b;
 double c;
-printf("Wprowadź kwote początkową:");
-
+    printf("Wprowadź kwote początkową:");
     scanf("%lf",&x);
+
     printf("Przez ile lat będzie na lokacie:");
-    scanf("%lf",&b);
+    scanf("%d",&b);
+
     printf("Podaj oprocentowanie:");
     scanf("%lf",&c);
-    c=c/100;
-    while (b>0) {
-    b-=1;
-        int i=1;
-        i++;
-    c=c+c*c;
-        x=x+(c*x);
-        printf(" nr%d    %lf     %lf \n",i,x,c);
-        i++;
+
+printf("Kapitał początkowy: %lf \n",x);
+printf("Oprocentowanie roczne: %lf \n",c);
+printf("ile lat:%d \n",b);
+
+int i=0;
+    double g=x;
+
+    double percent;
+while (b!=0){
+    b--;
+i++;
+    g = g*(1+c/100);
+percent = ((g-x)/x)*100;
+
+
+    if (percent==0) {
+        printf("%d %lf  %lf%% \n",i,g,c);
+
+    }else
+    printf("%d %lf  %lf%% \n",i,g,percent);
+
     }
-    printf("Kwota na koncu: %lf",x);
+
 }
