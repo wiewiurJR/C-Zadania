f2ff5962c98da5b7eaa399945ed7b88194a08924
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2026-01-10 09:27:51 +0100

    laby do zrobienia

---
+++ LAB3/zad3.c
@@ -0,0 +1,29 @@
+//
+// Created by bartosz on 1/9/2026.
+#include <stdio.h>
+
+int main()
+{
+    int n, i = 0;
+    float tablica[1000000];
+    float suma = 0.0;
+    printf("Ile liczb: ");
+    scanf("%d", &n);
+    size_t x = sizeof(tablica)/sizeof(tablica[0]);
+    if (n >x && n<=0) {
+        printf("Nie ma miejsc na liczby");
+        return 1;
+
+    }
+    printf("Podaj kolejne liczby:\n");
+    while(i<n)
+    {
+        scanf("%f", &tablica[i]);
+        i++;
+    }
+    for (i=2) {
+
+
+    }
+    return 0;
+}
---
+++ LAB3/zad4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//
+// Created by XYZ on 10.01.2026.
+//
+int main() {
+    float tablica[1000];
+    int n,i=0;
+    printf("Wprowadź liczby:");
+    scanf("%d",&n);
+    while(i<n)
+    {
+        scanf("%f", &tablica[i]);
+        i++;
+    }
+    
+}
