6abd78d87d76faa5f06a29c3f7308937f7c706a7
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2026-01-10 09:07:12 +0100

    zad2 poprawione

--- LAB3/zad2.c
+++ LAB3/zad2.c
@@ -7,12 +7,12 @@
 int main()
 {
     int n, i = 0;
-    float tablica[6];
+    float tablica[10];
     float suma = 0.0;
     printf("Ile liczb: ");
     scanf("%d", &n);
     size_t x = sizeof(tablica)/sizeof(tablica[0]);
-    if (n >x && n!=0) {
+    if (n >x && n<=0) {
         printf("Nie ma miejsc na liczby");
         return 1;
 
@@ -23,7 +23,7 @@ int main()
         scanf("%f", &tablica[i]);
         i++;
     }
-    for (i=n; i>=0; i--) {
+    for (i=n-1; i>=0; i--) {
 
         printf("%f\n", tablica[i]);
     }
