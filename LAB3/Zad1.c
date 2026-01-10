c8148363851d255144604cf7937b5828b6226ff2
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2026-01-10 09:03:47 +0100

    zad2

--- LAB3/Zad1.c
+++ LAB3/Zad1.c
@@ -1,23 +1,38 @@
+#include <math.h>
 #include <stdio.h>
-//
-// Created by bartosz on 1/9/2026.
-//#include <stdio.h>
-int main()
-{
+int main() {
     int n, i = 0;
-    float tablica[]={};
+    float tablica[10];
     float suma = 0.0;
+    float min,max,odchylenie=0.f;
     printf("Ile liczb: ");
     scanf("%d", &n);
+    size_t x = sizeof(tablica)/sizeof(tablica[0]);
+    if (n >x && n!=0) {
+        printf("Nie ma miejsc na liczby");
+        return 1;
+
+    }
     printf("Podaj kolejne liczby:\n");
+
     while(i<n)
     {
         scanf("%f", &tablica[i]);
         i++;
     }
-    for(i=0; i<n; i++)
+    for(i=0; i<n; i++) {
         suma = suma + tablica[i];
+        min = tablica[0];
+        if (min>tablica[i]) min = tablica[i];
+        if (max<tablica[i]) max = tablica[i];
+    }
+    float suma_od = 0.f;
+  for (i=0;i<n;i++) {
+      suma_od += (tablica[i] - (suma/n))*(tablica[i]-(suma/n));
+  }
+    odchylenie = sqrt(suma_od/(n));
     printf("Suma = %f\n", suma);
     printf("Srednia = %f\n", suma/n);
+    printf("Odchylenie standardowe = %f\n", odchylenie);
     return 0;
 }
--- LAB3/zad2.c
+++ LAB3/zad2.c
@@ -7,19 +7,25 @@
 int main()
 {
     int n, i = 0;
-    float tablica[1000];
+    float tablica[6];
     float suma = 0.0;
     printf("Ile liczb: ");
     scanf("%d", &n);
+    size_t x = sizeof(tablica)/sizeof(tablica[0]);
+    if (n >x && n!=0) {
+        printf("Nie ma miejsc na liczby");
+        return 1;
+
+    }
     printf("Podaj kolejne liczby:\n");
     while(i<n)
     {
         scanf("%f", &tablica[i]);
         i++;
     }
-    for(i=0; i<n; i++)
-        suma = suma + tablica[i];
-    printf("Suma = %f\n", suma);
-    printf("Srednia = %f\n", suma/n);
+    for (i=n; i>=0; i--) {
+
+        printf("%f\n", tablica[i]);
+    }
     return 0;
 }
