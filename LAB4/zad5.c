2eb66fc2ad2b4a628080c5a481f350d70636b0d5
wiewiurJR <131709268+wiewiurJR@users.noreply.github.com>
2026-01-30 21:27:23 +0000

    zad5

--- LAB4/zad5.c
+++ LAB4/zad5.c
@@ -1,39 +1,29 @@
-//
-// Created by bartosz on 1/12/2026.
-//
-#include <windows.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <windows.h>
 
-int czy_pierwsza(int n ) {
-    if (n<=1)return 0;
-    for (int i =2; i<n; i++) {
-        if (n%i==0)return 0;
+int czy_pierwsza(int N) {
+if (N<1)
+    return 0;
+    if (N==1)
         return 1;
-    }
-}
+
+        for (int i =2;  i*i<=N; i++)
+            if (N%i==0) {
+                return 0;
+            }
+    return 1;
+};
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    int n;
-
-    printf("Podaj Liczbę");
-    scanf("%d",&n);
-    int Tablica[n];
-
-    for (int i=0; i<n; i++) {
-        if (Tablica[i]==1) {
-            printf("%d",i);
-        }else {
-            printf("Liczba nie jest pierwsza");
-        }
+int n;
+    printf("Wpisz liczbę");
+scanf("%d",&n);
+printf("N=%d \n",n);
+    printf("Liczby pierwsze z zakresu od 1 do %d \n",n);
+for (int i=0; i<=n; i++) {
+    if (czy_pierwsza(i)){
+        printf("%d \n",i);
     }
-
-
-    return 0;
 }
-
-
-
-
-
+};
